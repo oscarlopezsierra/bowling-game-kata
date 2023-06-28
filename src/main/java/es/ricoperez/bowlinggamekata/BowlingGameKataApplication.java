eb074f3b2182598a0ad3a078ad4842ae4cc37a6d
Óscar Jesús López Sierra <oscar@ricoperez.es>
2023-06-28 08:11:29 +0200

    Initial commit

---
+++ src/main/java/es/ricoperez/bowlinggamekata/BowlingGameKataApplication.java
@@ -0,0 +1,13 @@
+package es.ricoperez.bowlinggamekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingGameKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlingGameKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ricoperez/bowlinggamekata/BowlingGameKataApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ricoperez.bowlinggamekata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingGameKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
